6c43afe4f6f5743f02f20fe97c5e17d7f89c7131
unknown <misbahak5010@gmail.com>
2022-05-30 14:30:59 -0400

    initial push

---
+++ src/components/ExpenseItem.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class ExpenseItem extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <h2>Expense Item</h2>
+        );
+    }
+}
+ 
+export default ExpenseItem;
