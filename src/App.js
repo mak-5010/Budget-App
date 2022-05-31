1b98c87e8721a3a48b229866589ccdc2e3a6f633
Muhammed Misbah A K <misbahak5010@gmail.com>
2022-05-31 15:45:59 +0530

    Started 40 - Composition

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.52,
+      date: new Date(2021, 2, 22),
+    },
+    {
+      id: "e2",
+      title: "TV",
+      amount: 2994.52,
+      date: new Date(2022, 5, 12),
+    },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 424,
+      date: new Date(2023, 11, 9),
+    },
+  ];
   return (
     <div>
       <h2>Let's get started !</h2>
-      <ExpenseItem />
+      <Expenses items={expenses} />
     </div>
   );
 }
